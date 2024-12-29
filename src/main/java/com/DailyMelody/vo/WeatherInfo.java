43ff87af54fc3c6f5263dcf3b206e98ebb68ca7d
Bernard203 <221250145@smail.nju.edu.cn>
2024-12-29 21:47:55 +0800

    fixed

--- src/main/java/com/DailyMelody/vo/WeatherInfo.java
+++ src/main/java/com/DailyMelody/vo/WeatherInfo.java
@@ -16,10 +16,10 @@ public class WeatherInfo {
     private String sunrise;
     private String sunset;
 
-    public WeatherInfo(String temperatureLow, String temperatureHigh, String unknown, String fx, String fl, String sunrise, String sunset) {
+    public WeatherInfo(String temperatureLow, String temperatureHigh, String wea, String fx, String fl, String sunrise, String sunset) {
         this.temperatureLow = temperatureLow;
         this.temperatureHigh = temperatureHigh;
-        this.weather = unknown;
+        this.weather = wea;
         this.fx = fx;
         this.fl = fl;
         this.sunrise = sunrise;
